27b490c6878912e708b7456b50ccb0d48103f78c
fernancumez <fernancumez@gmail.com>
2020-12-09 21:39:47 -0600

    middlewares and settings

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,31 @@
 import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 export class App {
   app: Application;
 
-  constructor() {
+  constructor(private port?: number | string | undefined) {
     this.app = express();
+    this.settings();
+    this.middlewares();
   }
 
-  async listen() {
+  settings() {
+    this.app.set("port", process.env.PORT || this.port || 4000);
+  }
+
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  async listen(): Promise<void> {
     try {
-      await this.app.listen(3000);
-      console.log(`Server on port ${3000}`);
+      await this.app.listen(this.app.get("port"));
+      console.log(`Server on port ${this.app.get("port")}`);
     } catch (err) {
       console.error(err);
     }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { App } from "./app";
+import { config } from "dotenv";
+
+process.env.NODE_ENV !== "production" ? config() : "";
 
 const main = async () => {
   try {
-    const app = new App();
+    const app = new App(3000);
 
     await app.listen();
   } catch (err) {
