de51c07195be88e012aaa54b615d8e2d162c22a7
fernancumez <fernancumez@gmail.com>
2020-12-10 18:35:52 -0600

    mysql enviroment configured

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { App } from "./app";
 import { config } from "dotenv";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
 
 process.env.NODE_ENV !== "production" ? config() : "";
 
@@ -7,6 +9,8 @@ const main = async () => {
   try {
     const app = new App(3000);
 
+    // create typeorm connections
+    await createConnection();
     await app.listen();
   } catch (err) {
     console.error(err);
