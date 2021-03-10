b2e8e7e1fb71fa2f14991af999777cb8d0c29c6c
fernancumez <fernancumez@gmail.com>
2021-03-10 17:17:14 -0600

    updated typeorm connection (#6)

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,19 @@
-import express, { Express } from "express";
+import express, { Application } from "express";
 import postRoutes from "./routes/post.routes";
-import { Config } from "./config";
-import morgan from "morgan";
+import config from "./config";
 import cors from "cors";
 
 // Initializations
-const app: Express = express();
-const { PORT } = Config;
+const app: Application = express();
 
 //Settings
-app.set("port", PORT);
+
+app.set("port", config.PORT);
 
 // Middlewares
 app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json()); // parse application/json
-app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api/posts", postRoutes);
